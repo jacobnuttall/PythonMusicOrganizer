436c06f8adbecec5ed00c3a5c3c700995db752b5
Jacob Nuttall <jacob_nuttall@outlook.com>
2025-10-15 13:02:56 -0600

    Add files via upload

--- organizer.py
+++ organizer.py
@@ -1,4 +1,6 @@
 # This file is part of my PythonMusicOrganizer project
+# 
+# MIT License
 # Copyright (c) 2025 Jacob Nuttall. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining
@@ -11,6 +13,15 @@
 #
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 
 import os 
 import shutil
